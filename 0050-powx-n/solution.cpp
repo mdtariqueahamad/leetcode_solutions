226f26c0e45fcecce237ea6af3e34b9e4e4eeb32
mdtariqueahamad <mdtariqbist@gmail.com>
2025-11-08 14:30:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (44.59%)

--- 0050-powx-n/solution.cpp
+++ 0050-powx-n/solution.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        double res = 1;
+        if(x <= 0 && !(n%2)) x = -x;
+        else if(x <= 0){
+            x = -x;
+            res = -1;
+        }
+        res *= exp(n * log(x));
+        return res;
     }
 };
