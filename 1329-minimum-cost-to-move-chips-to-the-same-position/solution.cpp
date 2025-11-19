bdd73332930b1686e23db3f297618e3aed57d1a2
mdtariqueahamad <mdtariqbist@gmail.com>
2025-11-19 18:35:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10 MB (40.20%)

---
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& position) {
+        int odd = 0, even = 0;
+        for (int x : position)
+            x % 2 ? odd++ : even++;
+        return min(odd, even);
+    }
+};
+
