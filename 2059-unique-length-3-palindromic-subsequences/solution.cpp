c3eb3b7883a65dc4e80961e26e72a7a93dba13c5
mdtariqueahamad <mdtariqbist@gmail.com>
2025-11-25 14:21:21 +0000

    Sync LeetCode submission Runtime - 32 ms (90.45%), Memory - 13.9 MB (93.63%)

---
+++ 2059-unique-length-3-palindromic-subsequences/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        int n = s.size();
+        int ans = 0;
+        for(char ch = 'a'; ch <= 'z'; ch++){
+            int L = -1, R = -1;
+            for(int i = 0; i < n; i++){
+                if(s[i] == ch){
+                    L = i;
+                    break;
+                }
+            }
+            for(int i = n - 1; i >= 0; i--){
+                if(s[i] == ch){
+                    R = i;
+                    break;
+                }
+            }
+
+            if(L != -1 && R != -1 && R - L >= 2){
+                bool seen[26] = {0};
+                for(int i = L + 1; i < R; i++){
+                    seen[s[i] - 'a'] = true;
+                }
+
+                for(int k = 0; k < 26; k++){
+                    if(seen[k]) ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
