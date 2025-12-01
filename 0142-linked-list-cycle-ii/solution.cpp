f8b9b22c4fb8bb515be52b81c99b7072d73b46b8
mdtariqueahamad <mdtariqbist@gmail.com>
2025-12-01 14:21:42 +0000

    Sync LeetCode submission Runtime - 3 ms (98.11%), Memory - 11.1 MB (99.21%)

--- 0142-linked-list-cycle-ii/solution.cpp
+++ 0142-linked-list-cycle-ii/solution.cpp
@@ -3,28 +3,20 @@ public:
     ListNode *detectCycle(ListNode *head) {
         if(!head) return nullptr;
 
-        ListNode* slow = head;
-        ListNode* fast = head;
-
-        // Step 1: Detect cycle
+        ListNode *slow = head, *fast = head;
         while(fast && fast->next) {
             slow = slow->next;
             fast = fast->next->next;
-
             if(slow == fast) break;
         }
-
-        // No cycle
         if(!fast || !fast->next) return nullptr;
-
-        // Step 2: Find start of cycle
         slow = head;
         while(slow != fast) {
             slow = slow->next;
             fast = fast->next;
         }
 
-        return slow; // This is the node where the cycle starts
+        return fast;
     }
 };
 
