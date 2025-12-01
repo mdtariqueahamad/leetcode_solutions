e6025190c70e69cc9a3a50a093bbc2f9a418439b
mdtariqueahamad <mdtariqbist@gmail.com>
2025-12-01 14:08:27 +0000

    Sync LeetCode submission Runtime - 8 ms (81.54%), Memory - 11.8 MB (53.58%)

--- 0141-linked-list-cycle/solution.cpp
+++ 0141-linked-list-cycle/solution.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-        std::unordered_set<ListNode*> visited;
-        ListNode* temp = head;
-
-        while(temp != nullptr) {
-            if(visited.count(temp)) {
-                return true;  // Cycle detected
-            }
-            visited.insert(temp);
-            temp = temp->next;
+    bool hasCycle(ListNode *head){
+        ListNode *slow = head, *fast = head;
+        while(fast != NULL && fast -> next != nullptr){
+        slow = slow -> next;
+        fast = fast -> next -> next;
+            if(slow == fast) return true;
         }
-
-        return false;  // No cycle
+        return false;
     }
 };
 
