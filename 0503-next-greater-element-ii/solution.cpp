926852308524f6ec4b86916919745e3b6364f606
mdtariqueahamad <mdtariqbist@gmail.com>
2025-12-08 16:52:06 +0000

    Sync LeetCode submission Runtime - 7 ms (35.89%), Memory - 28.4 MB (63.87%)

---
+++ 0503-next-greater-element-ii/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        vector <int> res(nums.size(), -1);
+        stack <int> st;
+        int n = nums.size();
+        for(int i = 2*(n-1); i >= 0; i--){
+            while(!st.empty() && st.top() <= nums[i%n])
+            st.pop();
+            if(i < n && !st.empty() && st.top() > nums[i%n]) res[i%n] = st.top();
+            st.push(nums[i%n]);
+        }
+        return res;
+    }
+};
