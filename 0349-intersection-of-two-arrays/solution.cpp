6c489e585f1d1e1e16db23ce9a3caee71cefe727
mdtariqueahamad <mdtariqbist@gmail.com>
2026-01-13 12:27:47 +0000

    Sync LeetCode submission Runtime - 1 ms (49.19%), Memory - 15 MB (7.98%)

--- 0349-intersection-of-two-arrays/solution.cpp
+++ 0349-intersection-of-two-arrays/solution.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector <int> res;
-        for(int x : nums1){
-            for(int y : nums2){
-                if( x == y && find(res.begin(), res.end(), x) == res.end())
-                res.push_back(x);
-            }
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2){
+        unordered_map <int,int> mpp1;
+        unordered_map <int,int> mpp2;
+        for(int x : nums1)
+        mpp1[x]++;
+
+        for(int x : nums2)
+        mpp2[x]++;
+        nums1.clear();
+        for(auto it : mpp1){
+            bool freq = mpp1[it.first] && mpp2[it.first];
+            if(freq)
+                nums1.push_back(it.first);
         }
-        return res;
+        return nums1;
     }
 };
