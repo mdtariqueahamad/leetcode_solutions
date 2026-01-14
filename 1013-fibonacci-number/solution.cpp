7d37b604dfd273deed4caa517363a5b208ff1b84
mdtariqueahamad <mdtariqbist@gmail.com>
2026-01-14 16:03:36 +0000

    Sync LeetCode submission Runtime - 10 ms (44.92%), Memory - 7.6 MB (88.52%)

--- 1013-fibonacci-number/solution.cpp
+++ 1013-fibonacci-number/solution.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     int fib(int n) {
-        int n1 = 0, n2 = 1, temp;
-        if(n == 0 || n == 1){
-            (n == 0) ? temp = n1 : temp = n2;
-        }
-        for(int i = 2; i <= n; i++){
-            temp = n2 + n1;
-            n1 = n2;
-            n2 = temp;
-        }
-        return temp;
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        return fib(n - 1) + fib(n - 2);
     }
 };
