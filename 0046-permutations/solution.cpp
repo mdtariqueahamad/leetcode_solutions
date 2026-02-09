3513a176fc62ed1a1fa1028a6e3f0927aef4b021
mdtariqueahamad <mdtariqbist@gmail.com>
2026-02-09 10:11:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.3 MB (98.26%)

---
+++ 0046-permutations/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    void perm(vector<int> &nums, int i, vector<vector<int>> &res){
+        if(i == nums.size()){
+            res.push_back(nums);
+            return;
+        }
+        for(int j = i; j < nums.size(); j++){
+            swap(nums[i], nums[j]);
+            perm(nums, i + 1, res);
+            swap(nums[i], nums[j]);
+        }
+
+    }
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        perm(nums, 0, res);
+        return res;
+    }
+};
