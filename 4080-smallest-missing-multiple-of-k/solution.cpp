e599b593297fb8d1d7d7f0069f6485360c6ccea7
mdtariqueahamad <mdtariqbist@gmail.com>
2026-08-25 15:28:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23.9 MB (94.80%)

---
+++ 4080-smallest-missing-multiple-of-k/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int f = 1;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == (k * f)) f++;
+        }
+        return k * f;
+    }
+};
