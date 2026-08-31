8a3d50b4cbe7385015b9b743d15195f6c66f7bd9
mdtariqueahamad <mdtariqbist@gmail.com>
2026-08-31 15:50:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12 MB (53.06%)

---
+++ 0047-permutations-ii/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> temp;
+    vector<bool> visited;
+    void solve(vector<int> &nums, int idx) {
+        if(nums.size() == idx) return;
+        if(temp.size() == nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+        if(0 < idx)
+        if(nums[idx-1] == nums[idx] && !visited[idx-1]){
+            solve(nums, idx+1);
+            return;
+        }
+        if(!visited[idx]){
+        temp.push_back(nums[idx]);
+        visited[idx] = true;
+        solve(nums, 0);
+        temp.pop_back();
+        visited[idx] = false;
+        }
+        solve(nums, idx+1);
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        visited = vector<bool>(nums.size(), false);
+        solve(nums, 0);
+        return ans;
+    }
+};
