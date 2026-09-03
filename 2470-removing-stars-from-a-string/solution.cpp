2077b31937893aac431560d2bacdb4b35cae2c13
mdtariqueahamad <mdtariqbist@gmail.com>
2026-09-03 14:59:54 +0000

    Sync LeetCode submission Runtime - 15 ms (76.42%), Memory - 28.3 MB (77.45%)

---
+++ 2470-removing-stars-from-a-string/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char> st;
+
+        for(char c: s)
+            if('*' == c) st.pop();
+            else st.push(c);
+
+        s.clear();
+        while(!st.empty()){
+            s += st.top();
+            st.pop();
+        }
+
+        reverse(s.begin(), s.end());  // O(n)
+        return s;
+    }
+};
